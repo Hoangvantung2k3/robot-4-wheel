c6f64aa41472cfd0759666bb48c69fc430d65fd3
Tung Hoang <vinhlieu1942@gmail.com>
2024-02-24 12:09:18 +0700

    first_code_upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/tung/robot_four/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/tung/robot_four/devel;/opt/ros/noetic'.split(';') if '/home/tung/robot_four/devel;/opt/ros/noetic' != '' else []
